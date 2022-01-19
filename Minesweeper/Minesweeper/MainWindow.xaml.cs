d47bea8bbffe4bec433dd18c0058c84709115fa6
ersidasb <63516886+ersidasb@users.noreply.github.com>
2022-01-19 15:26:45 +0200

    Add files via upload

---
+++ Minesweeper/Minesweeper/MainWindow.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Minesweeper.game;
+using System.IO;
+
+namespace Minesweeper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<BitmapImage> images = new List<BitmapImage>();
+        List<BitmapImage> smiles = new List<BitmapImage>();
+        List<List<Image>> boardButtons;
+        Game game;
+        int gameState = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            string[] files = Directory.GetFiles("images");
+            foreach (string s in files)
+            {
+                images.Add(new BitmapImage(new Uri(System.IO.Path.GetFullPath(s), UriKind.Absolute)));
+            }
+            files = Directory.GetFiles("smiles");
+            foreach (string s in files)
+            {
+                smiles.Add(new BitmapImage(new Uri(System.IO.Path.GetFullPath(s), UriKind.Absolute)));
+            }
+            newGame();
+        }
+
+        public void newGame()
+        {
+            Image image = new Image();
+            image.Source = smiles[0];
+            btnSmile.Content = image;
+            game = new Game(20, 50);
+
+            Tuple<List<List<int>>, int> result = game.getPlayBoard();
+            List<List<int>> playBoard = result.Item1;
+            gameState = result.Item2;
+            int size = playBoard.Count;
+            boardButtons = new List<List<Image>>();
+            board.RowDefinitions.Clear();
+            board.ColumnDefinitions.Clear();
+            board.IsEnabled = true;
+
+            for (int i = 0; i < size; i++)
+            {
+                List<Image> buttons = new List<Image>();
+                board.RowDefinitions.Add(new RowDefinition());
+                board.ColumnDefinitions.Add(new ColumnDefinition());
+                for (int j = 0; j < size; j++)
+                {
+                    image = new Image();
+                    image.SetValue(Grid.RowProperty, i);
+                    image.SetValue(Grid.ColumnProperty, j);
+                    image.Source = images[2];
+
+                    int r = i;
+                    int c = j;
+                    image.MouseLeftButtonUp += (snder, EventArgs) => { btn_LeftClick(snder, EventArgs, r, c); };
+                    image.MouseRightButtonUp += (snder, EventArgs) => { btn_RightClick(snder, EventArgs, r, c); };
+                    buttons.Add(image);
+                    board.Children.Add(image);
+                }
+                boardButtons.Add(buttons);
+            }
+
+            //drawBoard();
+        }
+
+        private void drawBoard()
+        {
+            Tuple<List<List<int>>, int> result = game.getPlayBoard();
+            List<List<int>> playBoard = result.Item1;
+            gameState = result.Item2;
+            int size = playBoard.Count;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    boardButtons[i][j].Source = images[playBoard[i][j] + 4];
+                }
+            }
+
+            if (gameState == -1)
+            {
+                Image image = new Image();
+                image.Source = smiles[2];
+                btnSmile.Content = image;
+                board.IsEnabled = false;
+            }
+            else if (gameState == 1)
+            {
+                Image image = new Image();
+                image.Source = smiles[1];
+                btnSmile.Content = image;
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (playBoard[i][j] == -2)
+                        {
+                            image = new Image();
+                            image.Source = images[0];
+                            boardButtons[i][j] = image;
+                        }
+                    }
+                }
+                board.IsEnabled = false;
+            }
+        }
+
+        private void disableBoard()
+        {
+            foreach(Button b in board.Children)
+            {
+                b.IsEnabled = false;
+            }
+        }
+
+        private void btn_LeftClick(object sender, EventArgs e, int i, int j)
+        {
+            game.move(i, j);
+            drawBoard();
+        }
+
+        private void btn_RightClick(object sender, EventArgs e, int i, int j)
+        {
+            game.flag(i, j);
+            drawBoard();
+        }
+
+        private void btnSmile_Click(object sender, RoutedEventArgs e)
+        {
+            newGame();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+    }
+}
---
+++ Minesweeper/Minesweeper/game/Game.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Minesweeper.game
+{
+    class Game
+    {
+        int size;
+        int mineCount;
+        int gameState;
+        Random rand = new Random();
+        List<List<int>> fullBoard = new List<List<int>>();
+        List<List<int>> playBoard = new List<List<int>>();
+        public Game(int size, int mineCount)
+        {
+            this.size = size;
+            this.mineCount = mineCount;
+            gameState = 0;
+            newBoard();
+        }
+
+        private void newBoard()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                List<int> fullRow = new List<int>();
+                List<int> playRow = new List<int>();
+                for (int j = 0; j < size; j++)
+                {
+                    fullRow.Add(0);
+                    playRow.Add(-2);
+                }
+                fullBoard.Add(fullRow);
+                playBoard.Add(playRow);
+            }
+        }
+
+        private void calculateNeighbours()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (fullBoard[i][j] != -1)
+                    {
+                        int top = i - 1;
+                        int bot = i + 1;
+                        int left = j - 1;
+                        int right = j + 1;
+                        if (top < 0)
+                            top = 0;
+                        if (bot > size - 1)
+                            bot = size - 1;
+                        if (left < 0)
+                            left = 0;
+                        if (right > size - 1)
+                            right = size - 1;
+                        for (int a = top; a <= bot; a++)
+                        {
+                            for (int b = left; b <= right; b++)
+                            {
+                                if (fullBoard[a][b] == -1)
+                                    fullBoard[i][j]++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void layMines(int x, int y)
+        {
+            int currmines = mineCount;
+            while (currmines > 0)
+            {
+                int a = rand.Next(size);
+                int b = rand.Next(size);
+                if (fullBoard[a][b] != -1 && x != a && y != b)
+                {
+                    fullBoard[a][b] = -1;
+                    currmines--;
+                }
+            }
+
+            calculateNeighbours();
+        }
+
+        public Tuple<List<List<int>>, int> getPlayBoard()
+        {
+            return Tuple.Create(playBoard, gameState);
+        }
+
+        public void move(int x, int y)
+        {
+            if (playBoard[x][y] == -4)
+                return;
+            bool contains = false;
+            foreach (List<int> row in fullBoard)
+            {
+                if (row.Contains(-1))
+                {
+                    contains = true;
+                    break;
+                }
+            }
+
+            if (!contains)
+                layMines(x, y);
+
+            if (fullBoard[x][y] != -1)
+            {
+                floodFill(x, y);
+                int squaresLeft = 0;
+                foreach (List<int> row in playBoard)
+                    foreach (int i in row)
+                        if(i != -2 && i != -4)
+                            squaresLeft++;
+                if (squaresLeft == size*size - mineCount)
+                    gameState = 1;
+                Console.WriteLine(squaresLeft);
+            }
+            else
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (fullBoard[i][j] == -1)
+                            playBoard[i][j] = fullBoard[i][j];
+                    }
+                }
+                gameState = -1;
+                playBoard[x][y] = -3;
+            }
+        }
+
+        private void floodFill(int x, int y)
+        {
+            if (x < 0 || x > size - 1)
+                return;
+            if (y < 0 || y > size - 1)
+                return;
+            if (playBoard[x][y] != -2)
+                return;
+            playBoard[x][y] = fullBoard[x][y];
+            if (fullBoard[x][y] == 0)
+            {
+                floodFill(x + 1, y);
+                floodFill(x, y + 1);
+                floodFill(x - 1, y);
+                floodFill(x, y - 1);
+
+                floodFill(x + 1, y + 1);
+                floodFill(x + 1, y - 1);
+                floodFill(x - 1, y + 1);
+                floodFill(x - 1, y - 1);
+            }
+        }
+
+        public void flag(int x, int y)
+        {
+            if(playBoard[x][y] == -2)
+            {
+                playBoard[x][y] = -4;
+            }
+            else if(playBoard[x][y] == -4)
+            {
+                playBoard[x][y] = -2;
+            }
+        }
+    }
+}
